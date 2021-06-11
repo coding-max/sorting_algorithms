1439b0f970c3db5d79859da399b28725daa79aea
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-11 20:14:41 -0300

    updated header file and auxiliar functions

---
+++ aux_functions.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * swap - swap to integers in the array
+ * @array: the array to be sorted
+ * @a: item to swap
+ * @b: item to swap
+ */
+void swap(int *array, size_t a, size_t b)
+{
+	int tmp = array[a];
+
+	array[a] = array[b];
+	array[b] = tmp;
+}
--- sort.h
+++ sort.h
@@ -22,8 +22,24 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* auxiliar functions */
+void swap(int *array, size_t a, size_t b);
+
+/* mandatory tasks functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+/* advanced tasks functions */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
